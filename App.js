96d76b8b888813cff1d40be41174f1fa4b374b23
Matthieu TIRELLI <matthieu@wisembly.com>
2021-08-22 14:32:04 +0200

    Add router

--- App.js
+++ App.js
@@ -1,30 +1,28 @@
 import React from "react";
 import * as eva from "@eva-design/eva";
-import { ApplicationProvider, IconRegistry, Text } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import Configuration from "core/configuration";
 import { ConfigurationContext, OrmContext } from "core/contexts";
 import orm from "core/orm";
 import Firewall from "containers/Firewall";
-import { StyleSheet } from "react-native";
+import Menu from "layouts/menu/Menu";
+import { NativeRouter, Route } from "react-router-native";
+import Transport from "layouts/transport";
 
+// eslint-disable-next-line react/display-name
 export default () => (
   <OrmContext.Provider value={orm}>
     <IconRegistry icons={EvaIconsPack} />
     <ConfigurationContext.Provider value={Configuration}>
       <ApplicationProvider {...eva} theme={{ ...eva.dark }}>
         <Firewall>
-          <Text style={styles.textLabel} status="control" category="h1">
-            Welcome !
-          </Text>
+          <NativeRouter>
+            <Route exact path="/" component={Menu} />
+            <Route exact path="/transport" component={Transport} />
+          </NativeRouter>
         </Firewall>
       </ApplicationProvider>
     </ConfigurationContext.Provider>
   </OrmContext.Provider>
 );
-
-const styles = StyleSheet.create({
-  textLabel: {
-    color: "#000000",
-  },
-});
